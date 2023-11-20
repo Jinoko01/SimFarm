5f2995cfe07c99072f9b773e36e07f719eb070a6
Hwangyongjin <dswvgw6412@naver.com>
2023-11-20 14:51:52 +0900

    feat:960x540 Update index.jsx

--- src/pages/MyRoom/index.jsx
+++ src/pages/MyRoom/index.jsx
@@ -6,7 +6,33 @@ const MyRoomDiv = styled.div`
 `;
 
 const MyRoom = () => {
-  return <MyRoomDiv className="wrapping"></MyRoomDiv>;
+  return (
+    <MyRoomDiv className="wrapping">
+      <div>
+        <h2>상세 정보</h2>
+        <div>
+          <div>동물 이미지</div>
+          <div>
+            <p>이름:</p>
+            <p>종류:</p>
+          </div>
+        </div>
+        <p>키:</p>
+        <p>몸무게:</p>
+        <p>나이:</p>
+        <p>성별:</p>
+        <p>특징:</p>
+        <p>매력:</p>
+        <p>애정도:</p>
+        <div>
+          <button>먹이</button>
+          <button>치장</button>
+          <button>돌보기</button>
+        </div>
+      </div>
+      <div></div>
+    </MyRoomDiv>
+  );
 };
 
 export default MyRoom;
