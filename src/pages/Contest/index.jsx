59d04e328125674afa162527f27945fab7caa4c9
Hwangyongjin <dswvgw6412@naver.com>
2023-11-23 12:07:21 +0900

    feat: release

--- src/pages/Contest/index.jsx
+++ src/pages/Contest/index.jsx
@@ -4,10 +4,31 @@ import styled from "styled-components";
 
 const ContestDiv = styled.div`
   background-image: url(${process.env.PUBLIC_URL + "image/contest.png"});
+
+  .contestTitle {
+    h1 {
+      margin: 0;
+      text-align: center;
+      padding-top: 10%;
+      font-size: 120px;
+      font-weight: 900;
+      letter-spacing: 20px;
+      background-image: linear-gradient(to top, green, red);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    }
+  }
 `;
 
 const Contest = () => {
-  return <ContestDiv className="wrapping"></ContestDiv>;
+  return (
+    <ContestDiv className="wrapping">
+      <div className="contestTitle">
+        <h1>동물 컨테스트</h1>
+      </div>
+    </ContestDiv>
+  );
 };
 
 export default Contest;
