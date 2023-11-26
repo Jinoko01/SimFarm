078323f3a2f564ebd6d5b06da1c467b411d8faaa
Kyxxn <gywns626@naver.com>
2023-11-26 21:21:45 +0900

    test

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import "../../style/GlobalCSS.scss";
 
