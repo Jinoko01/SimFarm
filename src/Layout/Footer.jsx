fd5fe4a1a1452ed9169ffae3b71e66e21ce178bb
Hwangyongjin <dswvgw6412@naver.com>
2023-12-01 13:49:12 +0900

    feat: Add Footer Content

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,11 +1,70 @@
 import styled from "styled-components";
+import "../style/GlobalCSS.scss";
+import { useState } from "react";
+import MyroomDescription from "./MyroomDescription";
+import StoreDescription from "./StoreDescription";
+import ContestDescription from "./ContestDescription";
 
 const FooterDiv = styled.div`
   background-color: #e9e9a1;
 `;
 
+const ContentDiv = styled.div`
+  width: 50%;
+  margin: 0 auto;
+
+  .title {
+    text-align: center;
+    margin-top: 3%;
+    margin-bottom: 10%;
+  }
+
+  p {
+    margin: 0;
+    text-align: center;
+  }
+
+  .control,
+  .pageName {
+    font-weight: 900;
+  }
+`;
+
+const CategoryBtn = styled.div`
+  text-align: center;
+  button {
+    margin: 4% 5%;
+  }
+`;
+
 const Footer = () => {
-  return <FooterDiv>Footer</FooterDiv>;
+  const [category, setCategory] = useState("myroom");
+  return (
+    <FooterDiv>
+      <ContentDiv>
+        <p className="nonDisplay">.</p>
+        <h1 className="title">SimFarm 게임방법</h1>
+        <p>
+          SimFarm 게임은 플레이어가 직접 동물들을 케어하면서 최고의 동물로
+          키워내는 게임입니다.
+        </p>
+        <p>동물의 매력과 애정도에 따라 다양한 엔딩이 연출됩니다.</p>
+        <br />
+        <p className="control">조작 방법</p>
+        <p>마우스</p>
+        <CategoryBtn>
+          <button onClick={() => setCategory("myroom")}>마이룸</button>
+          <button onClick={() => setCategory("store")}>상점</button>
+          <button onClick={() => setCategory("game")}>게임</button>
+          <button onClick={() => setCategory("contest")}>대회</button>
+        </CategoryBtn>
+        {category === "myroom" ? <MyroomDescription /> : null}
+        {category === "store" ? <StoreDescription /> : null}
+        {category === "game" ? <MyroomDescription /> : null}
+        {category === "contest" ? <ContestDescription /> : null}
+      </ContentDiv>
+    </FooterDiv>
+  );
 };
 
 export default Footer;
---
+++ src/Layout/MyroomDescription.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const DescriptionDiv = styled.div`
+  min-height: 540px;
+`;
+
+const ImageDiv = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 4%;
+
+  img {
+    margin: 0;
+  }
+`;
+
+const MyroomDescription = () => {
+  return (
+    <DescriptionDiv>
+      <ImageDiv>
+        <img
+          src={`${process.env.PUBLIC_URL}/image/myroomDescription.png`}
+          alt="myroomDescription"
+          style={{ width: "100%" }}
+        />
+      </ImageDiv>
+      <p className="pageName">마이룸 페이지</p>
+      <p style={{ marginTop: "3%" }}>
+        마이룸 페이지에서 플레이어가 가지고 있는 동물을 관리할 수 있습니다.
+      </p>
+      <p>
+        오른쪽에 있는 동물리스트에서 하나의 동물을 클릭할 시 왼쪽에 동물에 관한
+        상세정보가 나오게 됩니다.
+      </p>
+      <p>
+        <b>먹이</b>버튼을 통해 먹이를 줄 수 있습니다.
+      </p>
+      <p>
+        <b>치장</b>버튼을 통해 악세서리를 장착할 수 있습니다.
+      </p>
+      <p>
+        <b>돌보기</b>버튼을 통해 동물을 보살펴 줄 수 있습니다.
+      </p>
+    </DescriptionDiv>
+  );
+};
+
+export default MyroomDescription;
